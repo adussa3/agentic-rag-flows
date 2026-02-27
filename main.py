50811591cbda02b0a5a0bb78c8e5ebff5781d6da
Anunoy Dussa <adussa3@gmail.com>
2026-02-27 12:18:12 -0600

    Called graph in main

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ load_dotenv()
 # import Graph
 from graph.build_graph import graph
 
-if __name__ == '__main__':
+if __name__ == "__main__":
   print("Hello Advanced RAG")
-  print(graph.invoke(input={"question": "what is agent memory?"}))
+  # print(graph.invoke(input={"question": "what is agent memory?"}))
+  print(graph.invoke(input={"question": "how to make pizza?"}))
